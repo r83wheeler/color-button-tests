abdc867fc2437fde48be155234679917f0997d5f
Russ Wheeler <r83wheeler@gmail.com>
2022-05-31 10:32:41 -0400

    Test and code checkbox functinality

--- src/App.test.js
+++ src/App.test.js
@@ -32,5 +32,17 @@ test('initial conditions', () => {
   expect(checkbox).not.toBeChecked();
 });
 
+test('Checkbox disables button on first click and enables on second click', () => {
+  render(<App />);
+  const checkbox = screen.getByRole('checkbox');
+  const button = screen.getByRole('button');
+
+  fireEvent.click(checkbox);
+  expect(button).toBeDisabled();
+
+  fireEvent.click(checkbox);
+  expect(button).toBeEnabled();
+});
+
 
 
